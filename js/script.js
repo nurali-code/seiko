09bb85dff322c22bd4c138afb284700eeab561de
Nurali Davlatov <nur3.dav.97@gmail.com>
2024-01-03 23:04:39 +0500

    all done

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+
+});
+
