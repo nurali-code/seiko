6c2a10610cd0ebb55b9abb947532ad7ebb62e5dc
nurali-code <nur3.dav.97@gmail.com>
2024-01-04 11:33:15 +0500

    fixed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 $(document).ready(function () {
+    $(window).scroll(function () {
+        var header = $('header');
+        if ($(this).scrollTop() > 100) {
+            header.addClass('fixed');
+        } else { header.removeClass('fixed'); }
+    });
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top, }, 300,)
+    });
+
     $('.slider').slick({
         arrows: false,
         dots: false,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 3000,
         infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
